159f236dc061e2419c75d4fddbdfb4c5e343b676
sai sushvita chamarty <saisushvitha@gmail.com>
2026-04-04 21:40:45 -0300

    made change to origin

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://matrishield-frontend.vercel.app", "http://localhost:3000",],
     allow_methods=["*"],
     allow_headers=["*"],
 )
